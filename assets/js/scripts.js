139a06c6c6073a9d7cca7b57ca8fd9d1366b937e
Ilham Win10 <ilham.meidi@gmail.com>
2021-06-30 08:02:54 +0700

    finish footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -407,6 +407,16 @@ $('#filter_price button').click(function() {
   renderResult();
 });
 
+// Validate form
+var toastHTML = '<span>Message Sent</span><button onclick="M.Toast.dismissAll()" class="btn-icon waves-effect toast-action"><i class="material-icons">close</i></button>';
+$.validate({
+  form: "#contact_form",
+  onSuccess: function(form) {
+    M.toast({html: toastHTML});
+    return false;
+  }
+});
+
 var darkMode = 'false';
 if (typeof Storage !== 'undefined') { // eslint-disable-line
   darkMode = localStorage.getItem('nirwanaDarkMode') || 'false';
